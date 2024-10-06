25a8f2aeb9ad0eea98f76b736a2b9b13fe43d440
Varinia Butcher <varinia_b@hotmail.com>
2024-10-06 14:14:18 +0000

    Add category and cat models

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Cat(models.Model):
+    id = models.IntegerField(primary_key=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField
+    breed = models.CharField(max_length=50)
+    colour = models.CharField(max_length=50)
+    gender = models.CharField(max_length=6)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
