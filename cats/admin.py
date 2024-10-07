64fd0518e9fd55b05064aa2d29b58d38906aa859
Varinia Butcher <varinia_b@hotmail.com>
2024-10-07 19:57:48 +0000

    Add cats views and template

--- cats/admin.py
+++ cats/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Cat
 
 class CatAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age_if_known', 'breed', 'colour', 'gender', 'description', 'image_url', 'image')
+    list_display = (
+        'name', 
+        'age_if_known', 
+        'breed', 
+        'colour', 
+        'gender', 
+        'description', 
+        'image_url', 
+        'image'
+    )
+
+    ordering = ('name',)
 
 admin.site.register(Cat, CatAdmin)
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Cat
 
-# Create your views here.
+def all_cats(request):
+    """ a view to show all of the cats, including sorting and search queries """
+
+    cats = Cat.objects.all()
+
+    context = {
+        'cats': cats,
+    }
+
+    return render(request, 'cats/cats.html', context)
