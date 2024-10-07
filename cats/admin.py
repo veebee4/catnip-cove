81e60a1865eac0358f7c07d3a6e2bf428dad8bf5
Varinia Butcher <varinia_b@hotmail.com>
2024-10-07 19:30:18 +0000

    Added female cats to database

--- cats/admin.py
+++ cats/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Cat
 
-admin.site.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age_if_known', 'breed', 'colour', 'gender', 'description', 'image_url', 'image')
+
+admin.site.register(Cat, CatAdmin)
--- cats/models.py
+++ cats/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+]
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=50)
-    age = models.IntegerField
+    age_if_known = models.CharField(max_length=10)
     breed = models.CharField(max_length=50)
     colour = models.CharField(max_length=50)
-    gender = models.CharField(max_length=6)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
