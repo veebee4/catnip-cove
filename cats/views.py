f7dfb53fb355df07b270d93d206710f21008712a
Varinia Butcher <varinia_b@hotmail.com>
2024-10-09 19:42:56 +0000

    Add search functionality

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Cat
 
 def all_cats(request):
     """ a view to show all of the cats, including sorting and search queries """
 
     cats = Cat.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse,('cats'))
+
+            queries = Q(name__icontains=query) | Q(breed__icontains=query) | Q(colour__icontains=query) | Q(gender__icontains=query)
+            cats = cats.filter(queries)
+
 
     context = {
         'cats': cats,
+        'search_term': query,
     }
 
     return render(request, 'cats/cats.html', context)
