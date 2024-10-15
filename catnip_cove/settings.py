cf53f3ceaf9623e24cc2da16de18be5eb3a90257
Varinia Butcher <varinia_b@hotmail.com>
2024-10-15 11:16:52 +0000

    Add more styling to donation form

--- catnip_cove/settings.py
+++ catnip_cove/settings.py
@@ -49,8 +49,10 @@ INSTALLED_APPS = [
     'cats',
     'donations',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
--- donations/views.py
+++ donations/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .forms import DonationForm
 from .models import Donation
 from cats.models import Cat
