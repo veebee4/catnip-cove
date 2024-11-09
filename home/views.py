96a4386004788f745705c0fe8080907c107b906a
Varinia Butcher <varinia_b@hotmail.com>
2024-11-09 16:49:14 +0000

    Add contact page with url, view and updated link on main nav

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ def index(request):
     """ a view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """ a view to return the contact page """
+
+    return render(request, 'home/contact.html')
