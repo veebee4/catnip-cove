18b314c7459b1b75629ca43cf292ccee16c733f9
Varinia Butcher <varinia_b@hotmail.com>
2024-11-11 19:35:38 +0000

    Add functionality to donation form to pick up cat id

--- donations/views.py
+++ donations/views.py
@@ -10,7 +10,12 @@ stripe.api_key = settings.STRIPE_API_KEY
 
 
 def donate(request):
-    return render(request, 'donations/donate.html')
+    # Get the cat_id if selected
+    cat_id = request.GET.get('cat_id')
+    cat = get_object_or_404(Cat, id=cat_id) if cat_id else None
+
+    # Render the donate page with the cat context
+    return render(request, 'donations/donate.html', {'cat': cat})
 
 
 def charge(request):
@@ -22,12 +27,20 @@ def charge(request):
         custom_amount = request.POST.get('custom-amount')
         amount = int(selected_amount) * 100 if selected_amount else int(custom_amount) * 100 if custom_amount else 0
 
+        cat_id = request.POST.get('cat_id')
+        cat = None
+        if cat_id:
+            cat = get_object_or_404(Cat, id=cat_id)
 
+        # Create the customer in Stripe
         customer = stripe.Customer.create(
             name=f"{request.POST['first_name']} {request.POST['last_name']}",
             email=request.POST['email'],
             source=request.POST['stripeToken']
         )
+
+        # Add a description for the donation
+        description = f"Donation for {cat.name}" if cat else "General Donation"
 
         charge = stripe.Charge.create(
 			customer=customer.id,
