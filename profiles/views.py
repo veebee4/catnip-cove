9e4e764f565cfef918647ca78876b86c96cffab3
Varinia Butcher <varinia_b@hotmail.com>
2024-11-16 15:41:29 +0000

    Add more detail to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,12 @@ from .models import UserProfile
 def profile(request):
     """ display users profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+    donations = profile.donations.all()
 
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
+        'donations': donations,
     }
 
     return render(request, template, context)
