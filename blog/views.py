642c387ec045f4fd85f4158b69fcde6d57a067c8
Varinia Butcher <varinia_b@hotmail.com>
2024-11-28 20:39:43 +0000

    Add functionality for comments on blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Post, Comment, Category
+from blog.forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by("-created_on") #obtain all posts in database
@@ -22,10 +24,26 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+        else: 
+            messages.error(request, "The details you have entered are invalid, please try again.")
+            return redirect('donate')
+        return HttpResponseRedirect(request.path_info)
+        
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "blog/detail.html", context)
