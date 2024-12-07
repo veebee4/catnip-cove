61b69160e7bb778fd200da6b844b2a4d8715cac8
Varinia Butcher <varinia_b@hotmail.com>
2024-12-07 16:13:25 +0000

    Fix widget for edit cat form not rendering

--- cats/forms.py
+++ cats/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Cat
 
 class CatForm(forms.ModelForm):
@@ -7,9 +8,12 @@ class CatForm(forms.ModelForm):
         model = Cat
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         cats = Cat.objects.all()
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
