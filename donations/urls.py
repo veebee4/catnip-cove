e606343be260015e5e98116ea73c9dd0f477e712
Varinia Butcher <varinia_b@hotmail.com>
2024-12-14 15:39:13 +0000

    Update webhooks and webhook handler along with appropriate url and settings

--- donations/urls.py
+++ donations/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('donate/', views.donate, name='donate'),
     path('donate/<int:cat_id>/', views.donate, name='donate_with_cat'),
     path('success/<str:donation_number>/', views.successMsg, name="success"),
+    path('wh/', webhook, name="webhook")
 ]
