b88ca695b9ec316b23cdf5449086709b2a9ecc77
Varinia Butcher <varinia_b@hotmail.com>
2024-12-18 16:11:37 +0000

    Update blog management to include functionality for new categories

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,46 @@ from django import forms
 from .widgets import CustomClearableFileInput
 from .models import Post, Comment, Category
 
+
+class BlogForm(forms.ModelForm):
+
+    new_category = forms.CharField(
+        max_length=100,
+        required=False,
+        label="New Category (optional)",
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter a new category"}),
+    )
+
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "form-control"}),
+    )
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'image', 'categories', 'new_category']
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        posts = Post.objects.all()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_category = cleaned_data.get('new_category')
+
+        # Create a new category if the user entered a new category name
+        if new_category:
+            # Check if the category already exists to avoid duplication
+            category, created = Category.objects.get_or_create(name=new_category)
+            if created:
+                cleaned_data['categories'] = [category]  # Add the new category to the post's categories
+            else:
+                self.add_error('new_category', 'This category already exists.')
+        return cleaned_data
+
+
 class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
@@ -14,15 +54,3 @@ class CommentForm(forms.Form):
             attrs={"class": "form-control", "placeholder": "Leave a comment!"}
         )
     )
-
-
-class BlogForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        posts = Post.objects.all()
