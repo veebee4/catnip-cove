4049d541ba38fd3baf950b60eb899843e5520b5e
Varinia Butcher <varinia_b@hotmail.com>
2025-01-29 19:51:53 +0000

    Update sendEmail.js and contact form to trigger toast on submission

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,26 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the form element
     const contactForm = document.getElementById("contactForm");
 
-    // Add an event listener for the submit event
     contactForm.addEventListener("submit", function (e) {
-        e.preventDefault(); // Prevent page refresh
+        e.preventDefault();
 
-        // Send the form data using EmailJS
         emailjs.sendForm('service_8psbgm8', 'template_a6e089t', contactForm)
             .then(
                 function (response) {
                     console.log('SUCCESS!', response.status, response.text);
-                    alert("Your message has been sent!");
-                    contactForm.reset(); // Reset the form after success
+                    showToast("Your message has been sent!", "success");
+                    contactForm.reset();
                 },
                 function (error) {
                     console.log('FAILED...', error);
-                    alert("Sorry, something went wrong. Please try again later.");
+                    showToast("Sorry, something went wrong. Please try again later.", "danger");
                 }
             );
     });
 });
-
-
-
